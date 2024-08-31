03ecf4ac79f3007ca14d3897c530d2d2cd3e4d6b
Jay-Nehra <NJ.NEHRA@GMAIL.COM>
2024-08-31 10:55:58 +0530

    feat (Agents): Implementation of Agent with Tavily Search Tool Calling.

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from Introduction.hello import LLM_profile_summary
+from ReachOut.agents.profile_search_agent import profile_url_search
 
-LLM_profile_summary()
+
+
+if __name__=='__main__':
+    
+    name = 'Jayant Nehra'
+    persons_detail = f"LinkedIn profile URL for: {name} who works at Fifty Five technologies pvt ltd."
+    
+    profile_url = profile_url_search(persons_detail)
+    print(profile_url)
+
+    # LLM_profile_summary()
